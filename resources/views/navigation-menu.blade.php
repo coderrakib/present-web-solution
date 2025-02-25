d921e14e4b21b99729a587255250a2ca11a35cb1
coderrakib <coderrakib768@gmail.com>
2025-02-25 11:45:59 -0500

    Profile navlink change

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,18 +5,18 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="@if(Auth::user()->type == 'admin'){{route('admin.dashboard')}} @else {{route('user.dashboard')}} @endif">
                         {{--<x-application-mark class="block h-9 w-auto" />--}}
                         <img class="block h-9 w-auto" src="{{ asset('assets/img/present_web_soluation_logo.png') }}" alt="Logo">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                {{--<div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>--}}
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <a href="@if(Auth::user()->type == 'admin'){{route('admin.dashboard')}} @else {{route('user.dashboard')}} @endif" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                        {{ __('Back to Dashboard') }}
+                    </a>
+                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
