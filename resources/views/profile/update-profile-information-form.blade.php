7574f114c2f412e0e1b2e7f291b4c152a17c776e
coderrakib <coderrakib768@gmail.com>
2025-02-25 08:52:45 -0500

    User profile pic upload

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -12,20 +12,13 @@
         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
             <div x-data="{photoName: null, photoPreview: null}" class="col-span-6 sm:col-span-4">
                 <!-- Profile Photo File Input -->
-                <input type="file" id="photo" class="hidden" wire:model.live="photo" x-ref="photo" x-on:change="
-                                    photoName = $refs.photo.files[0].name;
-                                    const reader = new FileReader();
-                                    reader.onload = (e) => {
-                                        photoPreview = e.target.result;
-                                    };
-                                    reader.readAsDataURL($refs.photo.files[0]);
-                            " />
+                <input type="file" id="photo" class="hidden" wire:model.live="photo" x-ref="photo" x-on:change="photoName = $refs.photo.files[0].name;const reader = new FileReader();reader.onload = (e) => {photoPreview = e.target.result;};reader.readAsDataURL($refs.photo.files[0]);" />
 
                 <x-label for="photo" value="{{ __('Photo') }}" />
 
                 <!-- Current Profile Photo -->
                 <div class="mt-2" x-show="! photoPreview">
-                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full size-20 object-cover">
+                    <img src="{{ asset($this->user->profile_photo_url) }}" alt="{{ $this->user->name }}" class="rounded-full size-20 object-cover">
                 </div>
 
                 <!-- New Profile Photo Preview -->
